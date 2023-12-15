91f8dae69281756c4a53b7f9b650f1ff01d5b013
zhanghao_gongsi <1518530094@qq.com>
2023-12-15 09:45:04 +0800

    格式调整；整体功能已自测，待测试人员进一步测试

--- app/src/main/java/com/hx/infusionchairplateproject/viewmodel/AllAppViewModel.kt
+++ app/src/main/java/com/hx/infusionchairplateproject/viewmodel/AllAppViewModel.kt
@@ -184,231 +184,231 @@ class AllAppViewModel : ViewModel() {
         }
     }
 
-private fun realGetPadApkList(type: String) {
-    val padApkList: Call<PadApkList> = netManager.requestApi.getPadApkList(type)
-    padApkList.enqueue(object : Callback<PadApkList> {
-        override fun onResponse(call: Call<PadApkList>, response: Response<PadApkList>) {
-            if (!response.isSuccessful) {
-                if (debug) Log.d(TAG, "onResponse: App信息获取异常")
-                return
-            }
-            if (response.code() != 200) {
-                if (debug) Log.d(TAG, "onResponse: App信息响应码异常 Code = ${response.code()}")
-                return
-            }
-
-            val padApkList = response.body()
-
-            if (padApkList == null) {
-                if (debug) Log.d(TAG, "onResponse: App信息响应为空")
-                return
-
-            }
-            if (padApkList.status != 200) {
-                if (debug) Log.d(TAG, "onResponse: App信息响应数据中的 status(code) ！= 200")
-                return
-            }
-
-            // 标志 : 请求成功
-            isPadApkInfoLoadSuccess = true
-
-            val data = padApkList.data
-            if (data.isEmpty()) {
-                if (debug) Log.d(TAG, "onResponse: App信息为空")
-            }
-
-            when (type) {
-                "视频" -> {
-                    _videoApkList.value = emptyList()
+    private fun realGetPadApkList(type: String) {
+        val padApkList: Call<PadApkList> = netManager.requestApi.getPadApkList(type)
+        padApkList.enqueue(object : Callback<PadApkList> {
+            override fun onResponse(call: Call<PadApkList>, response: Response<PadApkList>) {
+                if (!response.isSuccessful) {
+                    if (debug) Log.d(TAG, "onResponse: App信息获取异常")
+                    return
+                }
+                if (response.code() != 200) {
+                    if (debug) Log.d(TAG, "onResponse: App信息响应码异常 Code = ${response.code()}")
+                    return
                 }
 
-                "游戏" -> {
-                    _gameApkList.value = emptyList()
+                val padApkList = response.body()
+
+                if (padApkList == null) {
+                    if (debug) Log.d(TAG, "onResponse: App信息响应为空")
+                    return
+
+                }
+                if (padApkList.status != 200) {
+                    if (debug) Log.d(TAG, "onResponse: App信息响应数据中的 status(code) ！= 200")
+                    return
                 }
 
-                "绘画" -> {
-                    _paintApkList.value = emptyList()
-                }
-            }
+                // 标志 : 请求成功
+                isPadApkInfoLoadSuccess = true
 
-            for (i in 0 until data.size) {
-                if (data[i].apkUrl == null) {
-                    if (debug) Log.d(TAG, "onResponse: App信息中的 apkUrl 为空")
-                    continue
+                val data = padApkList.data
+                if (data.isEmpty()) {
+                    if (debug) Log.d(TAG, "onResponse: App信息为空")
                 }
-                val appInfo = AppInfo(data[i].id, data[i].name, data[i].icon, data[i].packageName, data[i].apkUrl)
+
                 when (type) {
                     "视频" -> {
-                        _videoApkList.value = _videoApkList.value.plus(appInfo)
+                        _videoApkList.value = emptyList()
                     }
 
                     "游戏" -> {
-                        _gameApkList.value = _gameApkList.value.plus(appInfo)
+                        _gameApkList.value = emptyList()
                     }
 
                     "绘画" -> {
-                        _paintApkList.value = _paintApkList.value.plus(appInfo)
+                        _paintApkList.value = emptyList()
+                    }
+                }
+
+                for (i in 0 until data.size) {
+                    if (data[i].apkUrl == null) {
+                        if (debug) Log.d(TAG, "onResponse: App信息中的 apkUrl 为空")
+                        continue
+                    }
+                    val appInfo = AppInfo(data[i].id, data[i].name, data[i].icon, data[i].packageName, data[i].apkUrl)
+                    when (type) {
+                        "视频" -> {
+                            _videoApkList.value = _videoApkList.value.plus(appInfo)
+                        }
+
+                        "游戏" -> {
+                            _gameApkList.value = _gameApkList.value.plus(appInfo)
+                        }
+
+                        "绘画" -> {
+                            _paintApkList.value = _paintApkList.value.plus(appInfo)
+                        }
                     }
                 }
             }
-        }
 
-        override fun onFailure(call: Call<PadApkList>, t: Throwable) {
-            if (debug) Log.d(TAG, "onFailure: t = $t")
-            isPadApkInfoLoadSuccess = false
-        }
-
-    })
-}
-
-private fun realInitApk() {
-    val padApkLists = netManager.requestApi.padApkLists
-    padApkLists.enqueue(object : Callback<PadApkLists> {
-        @SuppressLint("SdCardPath")
-        override fun onResponse(call: Call<PadApkLists>, response: Response<PadApkLists>) {
-            if (!response.isSuccessful) {
-                if (debug) Log.d(TAG, "onResponse: 所有App信息获取异常")
-                return
-            }
-            if (response.code() != 200) {
-                if (debug) Log.d(TAG, "onResponse: 所有App信息响应码异常 Code = ${response.code()}")
-                return
+            override fun onFailure(call: Call<PadApkList>, t: Throwable) {
+                if (debug) Log.d(TAG, "onFailure: t = $t")
+                isPadApkInfoLoadSuccess = false
             }
 
-            val padApkLists = response.body()
+        })
+    }
 
-            if (padApkLists == null) {
-                if (debug) Log.d(TAG, "onResponse: 所有App信息响应为空")
-                return
-            }
-            if (padApkLists.status != 200) {
-                if (debug) Log.d(TAG, "onResponse: 所有App信息响应数据中的 status(code) ！= 200")
-                return
-            }
+    private fun realInitApk() {
+        val padApkLists = netManager.requestApi.padApkLists
+        padApkLists.enqueue(object : Callback<PadApkLists> {
+            @SuppressLint("SdCardPath")
+            override fun onResponse(call: Call<PadApkLists>, response: Response<PadApkLists>) {
+                if (!response.isSuccessful) {
+                    if (debug) Log.d(TAG, "onResponse: 所有App信息获取异常")
+                    return
+                }
+                if (response.code() != 200) {
+                    if (debug) Log.d(TAG, "onResponse: 所有App信息响应码异常 Code = ${response.code()}")
+                    return
+                }
 
-            // 标志  ： 成功
-            isPadApkListLoadSuccess = true
+                val padApkLists = response.body()
 
-            val data = padApkLists.data
-            if (data.isEmpty()) {
-                if (debug) Log.d(TAG, "onResponse: APP 下载路径为空")
-                return
-            }
+                if (padApkLists == null) {
+                    if (debug) Log.d(TAG, "onResponse: 所有App信息响应为空")
+                    return
+                }
+                if (padApkLists.status != 200) {
+                    if (debug) Log.d(TAG, "onResponse: 所有App信息响应数据中的 status(code) ！= 200")
+                    return
+                }
 
-            // 服务器列表
-            serverAPK = data.map { it.substringAfterLast("/") }.toMutableList()
-            // 本地列表
-            localAPK = GeneralUtil.getFileNamesInDirectory("/sdcard/TripartiteApp/").toMutableList()
+                // 标志  ： 成功
+                isPadApkListLoadSuccess = true
 
-            val differentData = serverAPK.filterNot { it in localAPK }
-            for (apk in localAPK) {
-                if (apk !in serverAPK) {
-                    val file = File("/sdcard/TripartiteApp/$apk")
-                    if (file.exists()) {
-                        file.deleteRecursively()
+                val data = padApkLists.data
+                if (data.isEmpty()) {
+                    if (debug) Log.d(TAG, "onResponse: APP 下载路径为空")
+                    return
+                }
+
+                // 服务器列表
+                serverAPK = data.map { it.substringAfterLast("/") }.toMutableList()
+                // 本地列表
+                localAPK = GeneralUtil.getFileNamesInDirectory("/sdcard/TripartiteApp/").toMutableList()
+
+                val differentData = serverAPK.filterNot { it in localAPK }
+                for (apk in localAPK) {
+                    if (apk !in serverAPK) {
+                        val file = File("/sdcard/TripartiteApp/$apk")
+                        if (file.exists()) {
+                            file.deleteRecursively()
+                        }
                     }
                 }
-            }
-            localAPK.retainAll { it in serverAPK }
+                localAPK.retainAll { it in serverAPK }
 
-            if (differentData.isNotEmpty()) {
-                for (apkName in differentData) {
-                    // go to download
-                    realDownloadApk("https://llm-huxing.oss-cn-beijing.aliyuncs.com/attachment/$apkName")
+                if (differentData.isNotEmpty()) {
+                    for (apkName in differentData) {
+                        // go to download
+                        realDownloadApk("https://llm-huxing.oss-cn-beijing.aliyuncs.com/attachment/$apkName")
+                    }
                 }
+
+
             }
 
+            override fun onFailure(call: Call<PadApkLists>, t: Throwable) {
+                if (debug) Log.d(TAG, "onFailure: t = $t")
+                isPadApkListLoadSuccess = false
+            }
 
-        }
-
-        override fun onFailure(call: Call<PadApkLists>, t: Throwable) {
-            if (debug) Log.d(TAG, "onFailure: t = $t")
-            isPadApkListLoadSuccess = false
-        }
-
-    })
-}
-
-/**
- * 正式下载并更新下载进度
- * link method : see updateDownloadProgress(url: String, progress: Long)
- */
-@SuppressLint("SdCardPath")
-private fun realDownloadApk(url: String) {
-    // update app state
-    updateDownloadProgress(url, 0)
-    // add download task
-    DownloadMgr.getInstance().addTask(url, "/sdcard/TripartiteApp/${url.substringAfterLast("/")}", object : DownloadMgr.Callback() {
-        override fun onStart(url: String) {
-            updateDownloadProgress(url, 0)
-        }
-
-        override fun onProgress(url: String, progress: Long, total: Long) {
-            updateDownloadProgress(url, progress * 100 / total)
-        }
-
-        override fun onSuccess(url: String, l: Long) {
-            updateDownloadProgress(url, 100)
-            // start install app
-            installApp(url)
-        }
-
-        override fun onFailed(url: String, cancelled: Boolean, msg: String?) {
-            Toaster.showShort(msg)
-            updateDownloadProgress(url, -1)
-        }
-    })
-}
-
-/**
- * 安装指定APP
- * url 中包含文件名
- */
-private fun installApp(url: String) {
-    CommandTool.execSuCMD("pm install -r /sdcard/TripartiteApp/${url.substringAfterLast("/")}")
-    updateDownloadProgress(url, (-1).toLong())
-}
-
-
-/**
- * url ： 指定App，唯一标识符
- * progress : 进度 ， 含义如下
- * 0 : 开始 ;
- * 0..99 : 进度 ;
- * 100 : 开始安装 ;
- * -1 : 默认状态（可用）
- */
-private fun updateDownloadProgress(url: String, progress: Long) {
-    val tmpVideoApkList = _videoApkList.value.toMutableList()
-    tmpVideoApkList.mapIndexed { index, appInfo ->
-        if (appInfo.url == url) {
-            tmpVideoApkList[index] = appInfo.copy(progress = progress)
-        }
+        })
     }
-    _videoApkList.value = tmpVideoApkList
 
-    val tmpGameApkList = _gameApkList.value.toMutableList()
-    tmpGameApkList.mapIndexed { index, appInfo ->
-        if (appInfo.url == url) {
-            tmpGameApkList[index] = appInfo.copy(progress = progress)
-        }
+    /**
+     * 正式下载并更新下载进度
+     * link method : see updateDownloadProgress(url: String, progress: Long)
+     */
+    @SuppressLint("SdCardPath")
+    private fun realDownloadApk(url: String) {
+        // update app state
+        updateDownloadProgress(url, 0)
+        // add download task
+        DownloadMgr.getInstance().addTask(url, "/sdcard/TripartiteApp/${url.substringAfterLast("/")}", object : DownloadMgr.Callback() {
+            override fun onStart(url: String) {
+                updateDownloadProgress(url, 0)
+            }
+
+            override fun onProgress(url: String, progress: Long, total: Long) {
+                updateDownloadProgress(url, progress * 100 / total)
+            }
+
+            override fun onSuccess(url: String, l: Long) {
+                updateDownloadProgress(url, 100)
+                // start install app
+                installApp(url)
+            }
+
+            override fun onFailed(url: String, cancelled: Boolean, msg: String?) {
+                Toaster.showShort(msg)
+                updateDownloadProgress(url, -1)
+            }
+        })
     }
-    _gameApkList.value = tmpGameApkList
 
-    val tmpPaintApkList = _paintApkList.value.toMutableList()
-    tmpPaintApkList.mapIndexed { index, appInfo ->
-        if (appInfo.url == url) {
-            tmpPaintApkList[index] = appInfo.copy(progress = progress)
-        }
+    /**
+     * 安装指定APP
+     * url 中包含文件名
+     */
+    private fun installApp(url: String) {
+        CommandTool.execSuCMD("pm install -r /sdcard/TripartiteApp/${url.substringAfterLast("/")}")
+        updateDownloadProgress(url, (-1).toLong())
     }
-    _paintApkList.value = tmpPaintApkList
-}
 
 
-override fun onCleared() {
-    super.onCleared()
-    viewModelJob.cancel()
-}
+    /**
+     * url ： 指定App，唯一标识符
+     * progress : 进度 ， 含义如下
+     * 0 : 开始 ;
+     * 0..99 : 进度 ;
+     * 100 : 开始安装 ;
+     * -1 : 默认状态（可用）
+     */
+    private fun updateDownloadProgress(url: String, progress: Long) {
+        val tmpVideoApkList = _videoApkList.value.toMutableList()
+        tmpVideoApkList.mapIndexed { index, appInfo ->
+            if (appInfo.url == url) {
+                tmpVideoApkList[index] = appInfo.copy(progress = progress)
+            }
+        }
+        _videoApkList.value = tmpVideoApkList
+
+        val tmpGameApkList = _gameApkList.value.toMutableList()
+        tmpGameApkList.mapIndexed { index, appInfo ->
+            if (appInfo.url == url) {
+                tmpGameApkList[index] = appInfo.copy(progress = progress)
+            }
+        }
+        _gameApkList.value = tmpGameApkList
+
+        val tmpPaintApkList = _paintApkList.value.toMutableList()
+        tmpPaintApkList.mapIndexed { index, appInfo ->
+            if (appInfo.url == url) {
+                tmpPaintApkList[index] = appInfo.copy(progress = progress)
+            }
+        }
+        _paintApkList.value = tmpPaintApkList
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 
 }
 
